1c656533c72df8b7b44022d1fe22a368b649623b
OFFLlNE <lauri.valja@transferwise.com>
2022-04-02 23:18:24 +0300

    Remove the now ignored directory node_modules

--- src/components/movielist/index.js
+++ src/components/movielist/index.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React from 'react';
 import styled from 'styled-components';
 
 import MovieItem from '../movieitem';
 
-export default function MovieList ({ movies, genres }) {
+export default function MovieList({ movies, genres, filteringKeyword }) {
   return (
     <MoviesWrapper>
-      { movies.map(movie => <MovieItem key={movie.id} movie={movie} genres={genres} />)}
+      {movies
+        .filter(
+          (movie) =>
+            movie.title
+              .toLowerCase()
+              .includes(filteringKeyword.toLowerCase()) ||
+            filteringKeyword === ''
+        )
+        .map((movie) => (
+          <MovieItem key={movie.id} movie={movie} genres={genres} />
+        ))}
     </MoviesWrapper>
-  )
+  );
 }
 
 const MoviesWrapper = styled.div`
   position: relative;
-`
+`;
--- src/pages/discover/index.js
+++ src/pages/discover/index.js
@@ -1,88 +1,96 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-import * as colors from "../../colors";
-import * as fetcher from "../../fetcher";
+import * as colors from '../../colors';
+import getMovies from '../../fetcher';
+import movies from './movies';
+import genres from './genres';
 
-import SearchFilters from "../../components/searchfilter";
-import MovieList from "../../components/movielist";
+import SearchFilters from '../../components/searchfilter';
+import MovieList from '../../components/movielist';
 
-export default class Discover extends React.Component {
-  constructor (props) {
-    super(props);
+const Discover = () => {
+  const [keyword, setKeyword] = useState('');
+  const [year, setYear] = useState(0);
+  const [results, setResults] = useState([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [genreOptions, setGenreOptions] = useState([]);
+  const [ratingOptions, setRatingOptions] = useState([
+    { id: 7.5, name: 7.5 },
+    { id: 8, name: 8 },
+    { id: 8.5, name: 8.5 },
+    { id: 9, name: 9 },
+    { id: 9.5, name: 9.5 },
+    { id: 10, name: 10 },
+  ]);
+  const [languageOptions, setLanguageOptions] = useState([
+    { id: 'GR', name: 'Greek' },
+    { id: 'EN', name: 'English' },
+    { id: 'RU', name: 'Russian' },
+    { id: 'PO', name: 'Polish' },
+  ]);
 
-    this.state = {
-      keyword: '',
-      year: 0,
-      results: [],
-      totalCount: 0,
-      genreOptions: [],
-      ratingOptions: [
-        { id: 7.5, name: 7.5 },
-        { id: 8, name: 8 },
-        { id: 8.5, name: 8.5 },
-        { id: 9, name: 9 },
-        { id: 9.5, name: 9.5 },
-        { id: 10, name: 10 }
-      ],
-      languageOptions: [
-        { id: 'GR', name: 'Greek' },
-        { id: 'EN', name: 'English' },
-        { id: 'RU', name: 'Russian' },
-        { id: 'PO', name: 'Polish' }
-      ]
-    };
-  }
+  useEffect(() => {
+    setResults(movies.results);
+    setGenreOptions(genres);
+    setTotalCount(movies.results.length);
+    // getMovies().then((result) => {
+    //   // Result actually returns us what we wanted
+    // });
+  }, []);
 
   // TODO: Preload and set the popular movies and movie genres when page loads
 
   // TODO: Update search results based on the keyword and year inputs
 
-  render () {
-    const { genreOptions, languageOptions, ratingOptions, totalCount, results } = this.state;
+  return (
+    <DiscoverWrapper>
+      <MobilePageTitle>Discover</MobilePageTitle>{' '}
+      {/* MobilePageTitle should become visible on mobile devices via CSS media queries*/}
+      <TotalCount>{totalCount} movies</TotalCount>
+      <MovieFilters>
+        <SearchFilters
+          genres={genreOptions}
+          ratings={ratingOptions}
+          languages={languageOptions}
+          searchMovies={(keyword, year) => this.searchMovies(keyword, year)}
+          onSearch={(e) => {
+            setKeyword(e);
+          }}
+        />
+      </MovieFilters>
+      <MovieResults>
+        <MovieList
+          movies={results || []}
+          genres={genreOptions || []}
+          filteringKeyword={keyword}
+        />
+      </MovieResults>
+    </DiscoverWrapper>
+  );
+};
 
-    return (
-      <DiscoverWrapper>
-        <MobilePageTitle>Discover</MobilePageTitle> {/* MobilePageTitle should become visible on mobile devices via CSS media queries*/}
-        <TotalCount>{totalCount} results</TotalCount>
-        <MovieFilters>
-          <SearchFilters 
-            genres={genreOptions} 
-            ratings={ratingOptions}  
-            languages={languageOptions}
-            searchMovies={(keyword, year) => this.searchMovies(keyword, year)}
-          />
-        </MovieFilters>
-        <MovieResults>
-          <MovieList 
-            movies={results || []}
-            genres={genreOptions || []}
-          />
-        </MovieResults>
-      </DiscoverWrapper>
-    )
-  }
-}
+export default Discover;
 
 const DiscoverWrapper = styled.main`
   padding: 35px;
-`
+`;
 
 const MovieResults = styled.div`
   display: inline-block;
   width: calc(100% - 295px);
-`
+`;
 
 const MovieFilters = styled.div`
   width: 280px;
   float: right;
   margin-top: 15px;
-`
+`;
 
 const MobilePageTitle = styled.h1`
   display: none;
-`
+`;
 
 const TotalCount = styled.strong`
   display: block;
-`
+`;
